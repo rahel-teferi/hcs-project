c0417267b4bf0680869e9c4e858e0ebd15466d04
Rahel Teferi <rahel.tem@gmail.com>
2025-03-28 11:29:08 +0100

    updating css for trivia page

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ function closeNav() {
   let sideNavEl = document.querySelector(".sideNav");
   sideNavEl.style.display = "none";
 }
+
+let catagoryEl = document.querySelector(".catagory");
+
+function openCatagory() {
+  if (catagoryEl.style.display == "block") {
+    catagoryEl.style.display = "none";
+  } else {
+    catagoryEl.style.display = "block";
+  }
+}
